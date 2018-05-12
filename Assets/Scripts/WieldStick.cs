ee358653b5e8a4c5d188ae41ebc059ed99ba41a8
missxa <aliona.kharchenko@gmail.com>
2018-05-12 14:12:37 +0200

    SteamVR integrated

---
+++ Assets/Scripts/WieldStick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WieldStick : MonoBehaviour {
+
+    public GameObject stickPrefab;
+    public Rigidbody attachPoint;
+
+    private GameObject stick;
+    private FixedJoint joint;
+
+
+	// Use this for initialization
+    // generate the xylophone stick in the hand (controller)
+	void Start () {
+        stick = GameObject.Instantiate(stickPrefab);
+        stick.transform.position = attachPoint.transform.position;
+        stick.transform.Rotate(new Vector3(0.0f, 0.0f, 80.0f));
+        stick.transform.Translate(new Vector3(0.0f, 0.2f, 0.0f));
+        joint = stick.AddComponent<FixedJoint>();
+        joint.connectedBody = attachPoint;
+	}
+	
+
+}
