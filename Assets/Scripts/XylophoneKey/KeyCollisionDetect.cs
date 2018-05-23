1495e98f6d46bf458b78d6fd26254eda290ce409
missxa <aliona.kharchenko@gmail.com>
2018-05-23 19:01:24 +0200

    delete some obsolete code files; try use trigger instead of collider for the keys

--- Assets/Scripts/XylophoneKey/KeyCollisionDetect.cs
+++ Assets/Scripts/XylophoneKey/KeyCollisionDetect.cs
@@ -13,6 +13,7 @@ public class KeyCollisionDetect : MonoBehaviour {
     private MusicalNote m_MusicalNote;
 
     private bool keyTouched = false;
+    private int keyTouchCount = 0;
     private float forceThreshold = 2.0f;
     #endregion // PRIVATE_MEMBER_VARIABLES
 
@@ -40,12 +41,33 @@ public class KeyCollisionDetect : MonoBehaviour {
             }
         }
     }
-    #endregion // PUBLIC_METHODS
 
-    #region PRIVATE_METHODS
-    private void OnCollisionExit(Collision collision)
+    void OnTriggerEnter(Collider other)
+    {
+        if (keyTouchCount <= 0)
+        {
+            keyVisualFeedback.MoveAndHighlightOnHit();
+            keyAudioFeedback.PlayKey(1000f);
+            m_MusicalNote.publishMusicalNote();
+            keyTouchCount += 1;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
     {
         keyTouched = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(keyTouchCount > 0)
+        {
+            keyTouchCount -= 1;
+        }
+        else
+        {
+            keyTouchCount = 0;
+        }
     }
     #endregion // PRIVATE_METHODS
 }
