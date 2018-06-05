1b2113cb583fd032ac3a5a44ce99a6d3fb2af549
missxa <aliona.kharchenko@gmail.com>
2018-06-05 13:57:37 +0200

    add moving objects in star wars scene

---
+++ Assets/Scripts/Init.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Init : MonoBehaviour {
+
+    // Use this for initialization
+    private GameObject xWing;
+    private GameObject cameraPos;
+    private GameObject tieFighters;
+    private GameObject xylophone;
+    private Vector3 xWingPos;
+    private Vector3 tieFightersPos;
+    private Vector3 xylophonePos;
+	void Start () {
+
+        xWing = GameObject.Find("XWings");
+        tieFighters = GameObject.Find("Tie fighters");
+        xylophone = GameObject.Find("Xylophone");
+
+        xWingPos = xWing.transform.localPosition;
+        tieFightersPos = tieFighters.transform.localPosition;
+        xylophonePos = xylophone.transform.localPosition;
+
+        cameraPos = GameObject.Find("Camera (head) (eye)");
+        xWing.transform.localPosition = new Vector3(cameraPos.transform.localPosition.x + xWingPos.x,
+            cameraPos.transform.localPosition.y + xWingPos.y,
+            cameraPos.transform.localPosition.z + xWingPos.z);
+        tieFighters.transform.localPosition = new Vector3(cameraPos.transform.localPosition.x + tieFightersPos.x,
+            cameraPos.transform.localPosition.y + tieFightersPos.y,
+            cameraPos.transform.localPosition.z + tieFightersPos.z);
+        xylophone.transform.localPosition = new Vector3(cameraPos.transform.localPosition.x + xylophonePos.x,
+            cameraPos.transform.localPosition.y + xylophonePos.y,
+            cameraPos.transform.localPosition.z + xylophonePos.z);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
---
+++ Assets/Scripts/MovingObjectsInStarWarsSkyBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObjectsInStarWarsSkyBox : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject laserShot;
+    public GameObject parent;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject xWing = GameObject.Find("XWingSelf");
+        float xWingPos;
+        xWingPos = xWing != null ? xWing.transform.localPosition.z : parent.transform.localPosition.z;
+        if (laserShot != null)
+        {
+            laserShot.transform.Translate(Vector3.up * 10F * Time.deltaTime);
+            float laserPos = laserShot.transform.localPosition.x;
+
+            if (xWingPos + 250 < laserPos)
+            {
+                laserShot.transform.localPosition = parent.transform.localPosition;
+            }
+        }
+        if (parent != null) {
+            //parent.transform.Translate(Vector3.right * 0.5F * Time.deltaTime);
+        }
+
+    }
+}
