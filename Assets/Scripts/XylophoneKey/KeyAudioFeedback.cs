cae5d5db4eaec753da03df8ee94a28a7a80c7257
missxa <aliona.kharchenko@gmail.com>
2018-06-16 11:23:56 +0200

    fixing the different unity version

--- Assets/Scripts/XylophoneKey/KeyAudioFeedback.cs
+++ Assets/Scripts/XylophoneKey/KeyAudioFeedback.cs
@@ -3,20 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-public class KeyAudioFeedback : MonoBehaviour {
+public class KeyAudioFeedback : MonoBehaviour
+{
 
-    private AudioSource keySound;
+    private AudioSource[] keySound;
 
     private float maxForce = 1000;
 
     // Use this for initialization
-    void Start () {
-        keySound = GetComponent<AudioSource>();
-	}
-	
-	
+    void Start()
+    {
+        keySound = GetComponents<AudioSource>();
+    }
+
+
 
     public void PlayKey(float force)
+    {
+        if (GameObject.FindGameObjectWithTag("cubeStick") != null)
+        {
+            playKey(keySound[0], force);
+        }
+        else
+        {
+            if (GameObject.FindGameObjectWithTag("roboyStick") != null)
+            {
+                playKey(keySound[1], force);
+            }
+            else
+            {
+                if (GameObject.FindGameObjectWithTag("lightsaberStick") != null)
+                {
+                    playKey(keySound[2], force);
+                }
+            }
+        }
+    }
+
+    private void playKey(AudioSource keySound, float force)
     {
         float vol = Mathf.Clamp(force / maxForce, 0.0f, 1.0f);
         keySound.volume = vol;
