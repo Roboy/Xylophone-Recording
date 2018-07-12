c8c7f2fdfa539126d558cedc0c145ac1bd26ab03
rainnymig <lego_cyp@163.com>
2018-07-12 15:39:31 +0200

    add a cassette and cassette player model built in blender and create a test scene

---
+++ Assets/Scripts/SongSystem/CassettePlayerManeger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CassettePlayerManeger : MonoBehaviour {
+
+    public GameObject m_indicator;
+    public KeyCode TestKeyCode;
+    public Vector3 ShootForce;
+
+    private GameObject m_Cassette;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_indicator.SetActive(true);
+        if (collision.transform.gameObject.tag == "Cassette")
+        {
+            m_Cassette = collision.transform.gameObject;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(TestKeyCode) && m_Cassette != null)
+        {
+            m_Cassette.GetComponent<Rigidbody>().AddForce(ShootForce);
+            m_indicator.SetActive(false);
+            m_Cassette = null;
+        }
+    }
+}
