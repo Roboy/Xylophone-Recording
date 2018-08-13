04da530a23818cb1d3bec1da2cffe60be49fc833
missxa <aliona.kharchenko@gmail.com>
2018-08-13 17:18:56 +0200

    last changes on second scene before merge

--- Assets/Scripts/WebCam.cs
+++ Assets/Scripts/WebCam.cs
@@ -4,12 +4,19 @@ public class WebCam : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
         WebCamDevice[] cameraDevices = WebCamTexture.devices;
+        int viveCameraIndex;
+        for (viveCameraIndex = 0; viveCameraIndex < cameraDevices.Length; viveCameraIndex++) {
+            if (cameraDevices[viveCameraIndex].name.Equals("HTC Vive")) {
+                break;
+            }
+        }
 
         Renderer rend = this.GetComponentInChildren<Renderer>();
 
         WebCamTexture myCamera = new WebCamTexture();
-        string cameraName = cameraDevices[2].name;
+        string cameraName = cameraDevices[viveCameraIndex].name;
         myCamera.deviceName = cameraName;
         rend.material.mainTexture = myCamera;
         myCamera.Play();
