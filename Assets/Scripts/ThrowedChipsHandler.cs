4bc4d3a669ba1cd01d9a92d190144c4d6c397343
arash <arash.kiani@tum.de>
2018-08-15 22:25:41 +0200

    Make it work for video

---
+++ Assets/Scripts/ThrowedChipsHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowedChipsHandler : MonoBehaviour {
+
+    private Vector3 firstPosition;
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 chipsPos = this.transform.localPosition;
+
+        chipsPos = new Vector3(chipsPos.x, chipsPos.y+1,
+            chipsPos.z);
+        GameObject controller = GameObject.FindGameObjectWithTag(TagsConstants.CONTROLLER_DEVICE);
+        if (Mathf.Abs(controller.transform.position.y - chipsPos.y) > 10) {
+            Destroy(this);
+
+
+        }
+    }
+    
+
+}
